78f4be384763353c7202be937490824567daffb4
Ziyauddin Shaik <shaikziyauddin189@gmail.com>
2024-07-21 23:30:54 +0530

    #bottom navigation bar

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,12 +1,42 @@
+import 'package:awesome_bottom_bar/awesome_bottom_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:nosh_assignment/view_models/recipe_view_model.dart';
 import 'package:nosh_assignment/views/discover_container.dart';
 import 'package:nosh_assignment/views/dish_of_day.dart';
 import 'package:stacked/stacked.dart';
 
-class HomeScreen extends StatelessWidget {
+const List<TabItem> items = [
+  TabItem(
+    icon: Icons.home,
+    title: 'Recipes',
+  ),
+  TabItem(
+    icon: Icons.calendar_month,
+    title: 'List',
+  ),
+  TabItem(
+    icon: Icons.library_books,
+    title: 'Nosh',
+  ),
+  TabItem(
+    icon: Icons.event,
+    title: 'Meal Plan',
+  ),
+  TabItem(
+    icon: Icons.account_box,
+    title: 'Profile',
+  ),
+];
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int visit = 0;
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<RecipeViewModel>.reactive(
@@ -34,6 +64,23 @@ class HomeScreen extends StatelessWidget {
                 DiscoverContainer(),
               ],
             ),
+          ),
+          bottomNavigationBar: BottomBarInspiredFancy(
+            items: items,
+            backgroundColor: Colors.white, //withOpacity(0.21),
+            color: Colors.grey,
+            colorSelected: Colors.deepOrangeAccent,
+            indexSelected: visit,
+            styleIconFooter: StyleIconFooter.dot,
+            onTap: (int index) => setState(() {
+              visit = index;
+              if (index == 1) {
+                //
+              }
+              if (index == 2) {
+                //
+              }
+            }),
           ),
         );
       },
